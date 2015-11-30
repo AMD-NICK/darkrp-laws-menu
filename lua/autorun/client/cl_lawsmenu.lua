568dddf23d221fdf640001e26e0cd715af0ff6d9
Mad Kernel <amd@default.im>
2015-11-30 14:48:26 +0200

    Refresh button and small optimization

--- lua/autorun/client/cl_lawsmenu.lua
+++ lua/autorun/client/cl_lawsmenu.lua
@@ -6,9 +6,6 @@ Description: Tired of looking for law boards? Well, now all you have to do is ty
 
 AddCSLuaFile()
 
-height = ScrH() -- useless (It's there just in case)
-width = ScrW() -- useless (It's there just in case)
-
 local function law_add()
 	Derma_StringRequest(
 		"Create New Law",
@@ -27,12 +24,15 @@ local function law_remove()
 	)
 end
 
+
 local function law_menu()
-	lawspls = DarkRP.getLaws()
+	local lawspls = DarkRP.getLaws()
+	local isMayor = LocalPlayer():getJobTable().mayor
+
 
 	local lmenu = vgui.Create( "DFrame" ) -- Main Menu
 	lmenu:SetSize( 530, 380 )
-	lmenu:SetPos( width/2, height/2 )
+	lmenu:Center()
 	lmenu:SetTitle( "Government Laws" )
 	lmenu:SetVisible( true )
 	lmenu:SetDraggable( true )
@@ -41,6 +41,7 @@ local function law_menu()
 	lmenu:Center()
 	lmenu.Paint = function( pnl, w, h ) draw.RoundedBox( 8, 0, 0, w, h, Color( 0, 0, 0, 245 ) ) end
 
+
 	local lclose = vgui.Create( "DButton", lmenu ) -- Close button
 	lclose:SetSize( 60, 25 )
 	lclose:SetPos( lmenu:GetWide() - lclose:GetWide(), 0 ) 
@@ -48,13 +49,14 @@ local function law_menu()
 	lclose:SetFont("Trebuchet24")
 	lclose:SetTextColor( Color( 255, 255, 255 ) )
 	lclose.Paint = function( pnl, w, h ) 
-		draw.RoundedBoxEx( 8, 0, 0, w, h, Color( 200, 40, 40 ),false,true,true,false)
+		draw.RoundedBoxEx( 8, 0, 0, w, h, Color( 200, 40, 40 ),false,true,false,false)
 	end
 	lclose.DoClick = function() lmenu:Remove() end
 
+
 	local llist = vgui.Create("DListView", lmenu)
 	llist:SetPos(3, 24 + 3)
-	llist:SetSize(lmenu:GetWide() - 6, lmenu:GetTall() - 24 - 6 - 30) -- width 524 (1600x1024)
+	llist:SetSize(lmenu:GetWide() - 6, lmenu:GetTall() - 24 - 6 - 30) -- width 524
 	llist:SetMultiSelect(false)
 	llist:AddColumn("Number")
 	llist:AddColumn("Law")
@@ -64,6 +66,8 @@ local function law_menu()
 		llist:AddLine(k,v).law = k
 	end
 	llist.OnRowRightClick = function( row, dat )
+		if !isMayor then return end
+
 		local law = llist:GetLine(llist:GetSelectedLine()).law
 		--local law = dat -- also you can use this method, but i want to do else
 
@@ -75,7 +79,7 @@ local function law_menu()
 	end
 
 
-	if LocalPlayer():getJobTable().mayor then
+	if isMayor then
 
 		local ladd = vgui.Create( "DButton" )
 		ladd:SetSize( 150, 25 )
@@ -110,11 +114,40 @@ local function law_menu()
 		lrem:SetText( "Remove Law" )
 		lrem:SetFont("Trebuchet24")
 		lrem:SetTextColor( Color( 255, 255, 255 ) )
-		lrem.DoClick = function() law_remove() end
-		function lrem:Paint( w, h ) -- Remove Laws Button Color
+		lrem.Paint = function( pnl, w, h ) -- Remove Laws Button Color
 			draw.RoundedBoxEx(8,0,0,w,h,Color(200,130,10),true,false,false,true)
 		end
+		lrem.DoClick = function()
+			if not llist:GetSelectedLine() then
+				law_remove()
+			else
+				local law = llist:GetLine(llist:GetSelectedLine()).law
+				RunConsoleCommand("say", "/removelaw" .. " ".. law)
+			end
+		end
 
+	end
+
+
+	local lref = vgui.Create( "DButton", lmenu ) -- Close button
+	lref:SetSize( 25, 25 )
+	lref:SetPos( lmenu:GetWide()-lclose:GetWide()-lref:GetWide()-2, 0 ) 
+	lref:SetText( "↻" )
+	lref:SetFont("Trebuchet24")
+	lref:SetTextColor( Color( 255, 255, 255 ) )
+	lref.Paint = function( pnl, w, h ) 
+		draw.RoundedBoxEx( 8, 0, 0, w, h, Color( 200, 40, 40 ),false,false,true,false)
+	end
+	-- I know, it's not a good way to reload data,
+	-- but I do not know other to refresh it.
+	-- Google not help me with that
+	lref.DoClick = function()
+		lawspls = DarkRP.getLaws()
+		llist:Clear()
+
+		for k,v in pairs(lawspls) do
+			llist:AddLine(k,v).law = k
+		end
 	end
 end
 
